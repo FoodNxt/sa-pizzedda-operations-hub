e2468c3c33260ca97c4ea280ff52663d251a6854
Francesco Carboni <admin@sapizzedda.it>
2025-11-25 00:19:42 +0000

    File changes

--- src/pages/Preparazioni.jsx
+++ src/pages/Preparazioni.jsx
@@ -177,7 +177,7 @@ export default function Preparazioni() {
                     >
                       {store.name}
                     </button>
-                }
+                  ))}
               </div>
             </div>
 
